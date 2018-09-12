e408a5686dea1f2975541b35fc563bceacd3da46
Busheng Xi <busheng.xi@yvw.com.au>
2018-09-12 14:21:18 +1000

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# React Setup
+
+A basic setup for React project with Babel, Webpacke and etc.
+
+# Get started
+
+0. Have NodeJS and Git installed.
+
+1. Create a folder e.g. `my-react-project`.
+2. Copy all content of this repo into `my-react-project`.
+3. Go to `my-react-project`, run `npm i`.
+4. Run `npm run dev`
+
---
+++ webpack.dev.config.js
@@ -0,0 +1,35 @@
+var path = require( 'path' );
+
+module.exports = {
+
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+
+        path: path.join( __dirname, 'dist' ),
+        filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+
+        contentBase: path.join(__dirname, 'dist' ),
+        compress: true,
+        port: 5000,
+        inline: false, /* Only disable auto-reload for browser, not code. */
+    },
+    module: {
+        
+        rules: [ {
+
+            test: /\.js[x]{0,1}$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+
+                loader: 'babel-loader',
+                options: {
+                    presets: [ 'react', 'env' ]
+                }
+            }
+        } ]
+    }
+};
