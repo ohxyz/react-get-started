7f9aca7fcb6f7e31dbe9d2ad35eed2249b0d2a0d
Busheng Xi <ohxyzcom@gmail.com>
2021-02-17 16:14:11 +1100

    update eol and other  minor changes

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,6 +8,7 @@ module.exports = {
     devServer: {
         compress: true,
         port: 5000,
+        // Does not give detailed error due to issues in webpack-dev-server
         overlay: true,
     },
     module: {
